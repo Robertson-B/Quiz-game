9621177c774071b9df772edb1e0524288472c9e4
Bean <brobertson@student.saintaug.nsw.edu.au>
2025-04-04 11:37:32 +1100

    added a readme file

---
+++ README.md
@@ -0,0 +1,34 @@
+# The Gauntlet
+
+Welcome to the greatest quiz in the world, designed to test your intellectual prowess all while providing a fun and engaging experience.
+
+It is a python based application that runs in the terminal aimed to be a fun quiz to help people test their knowledge on a variety of topics. There are no hints because they are only for cowards and fools.
+## Author
+
+- [Benjamin Robertson (Developer)](https://github.com/Robertson-B)
+
+
+
+## Appendix
+
+This is a quite simple and straight forward process.
+
+Just input the number of your choices and off you go.
+
+Test your skills and see how you do against the leaderboard.
+## Acknowledgements
+
+- Mr Fong (The Best Teacher I could ever hope  for)
+ - [ChrisBuilds (Terminal Text Effects)](https://github.com/ChrisBuilds)
+
+
+## Installation
+
+Install the required modules with pip
+
+```bash
+  pip install terminaltexteffects
+  pip install rich
+  pip install pygame
+```
+    
--- music.py
+++ music.py
@@ -1,35 +1,34 @@
 from pygame import mixer # Music engine
 
 def loading_music(): # Music for start of game
-    #mixer.init()
-    #mixer.music.load('loading.mp3')
-    #mixer.music.set_volume(0.2)
-    #mixer.music.play(loops = -1) 
-    pass
+    mixer.init()
+    mixer.music.load('loading.mp3')
+    mixer.music.set_volume(0.2)
+    mixer.music.play(loops = -1) 
+
 
 
 def quiz_theme(): # Jeopardy theme music
-    #mixer.init()
-    #mixer.music.load('quiz_theme.mp3')
-    #mixer.music.set_volume(1)
-    #mixer.music.play(loops = 1) 
-    pass
+    mixer.init()
+    mixer.music.load('quiz_theme.mp3')
+    mixer.music.set_volume(1)
+    mixer.music.play(loops = 1) 
+
 
 def stop_music(): # Stops all music
-    #mixer.init()
-    #mixer.music.stop()
-    pass
+    mixer.init()
+    mixer.music.stop()
+    
 
 def correct_answer(): # Nice ding sound
-    #mixer.init()
-    #mixer.music.load('correct_answer.mp3')
-    #mixer.music.set_volume(0.7)
-    #mixer.music.play(loops = 1)
-    pass
+    mixer.init()
+    mixer.music.load('correct_answer.mp3')
+    mixer.music.set_volume(0.7)
+    mixer.music.play(loops = 1)
+
 
 def wrong_answer(): # Buzzer sound
-    #mixer.init()
-    #mixer.music.load('wrong_answer.mp3')
-    #mixer.music.set_volume(0.6)
-    #mixer.music.play(loops = 1)
-    pass
+    mixer.init()
+    mixer.music.load('wrong_answer.mp3')
+    mixer.music.set_volume(0.6)
+    mixer.music.play(loops = 1)
